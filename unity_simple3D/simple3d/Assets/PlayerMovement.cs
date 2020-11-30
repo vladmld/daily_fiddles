cf0edda62a02e74309d4b9c16dd26959d3a95d1a
vladmld <vlad.moldoveanu@protonmail.com>
2020-11-30 11:27:27 +0200

    Continue with tutorial

--- unity_simple3D/simple3d/Assets/PlayerMovement.cs
+++ unity_simple3D/simple3d/Assets/PlayerMovement.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody cubeRigidBody;
+
+    public float forwardForce = 1500f;
+
+    public float sidewayForce = 500f;
+
+    private float _normalizedForwardForce;
+
+    private float _normalizedSidewayForce;
+
     void Start()
     {
-        
+        _normalizedForwardForce = forwardForce * Time.deltaTime;
+        _normalizedSidewayForce = sidewayForce * Time.deltaTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // check for input here and store them in variable and update in fixed update
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            cubeRigidBody.AddForce(0, 0, _normalizedForwardForce);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            cubeRigidBody.AddForce(-_normalizedSidewayForce, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            cubeRigidBody.AddForce(0, 0, -_normalizedForwardForce);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            cubeRigidBody.AddForce(_normalizedSidewayForce, 0, 0);
+        }
     }
 }
